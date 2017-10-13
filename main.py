1336f8b6ede4f63f043ff27bfecdc25b9a72a6bb
linwang929 <lin.wang@go.wustl.edu>
2017-10-13 01:35:16 -0500

    completed the first part of build-a-blog

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.String(2000))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route("/blog")
+def blog():
+    blogs = Blog.query.all()
+    return render_template('blog.html', title="Build a Blog", blogs=blogs)
+
+@app.route("/newpost", methods=['GET','POST'])
+def newpost():
+
+    if request.method == 'POST':
+        blog_title = request.form['title']
+        blog_body = request.form['body']
+        title_error = ''
+        body_error = ''
+
+        if blog_title == '':
+            title_error = 'Please add the title of the blog.'
+        if blog_body == '':
+            body_error = 'Please add the content of the blog.'
+
+        if not title_error and not body_error:
+            new_blog = Blog(blog_title, blog_body)
+            db.session.add(new_blog)
+            db.session.commit()
+            return redirect("/blog")
+        else:
+            return render_template('newpost.html', title="Add a Blog Entry",
+                title_error=title_error, body_error=body_error)
+
+    return render_template('newpost.html', title="Add a Blog Entry")
+
+
+
+if __name__ == '__main__':
+    app.run()
